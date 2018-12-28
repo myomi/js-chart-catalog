20657a38d009f1d7017c92e142e70d5472e65ab4
MITSUYA MYOUCHIN <m.myouchin@nakamura-joho.co.jp>
2018-12-28 16:09:50 +0900

    use setPositioner

--- src/highcharts/column.ts
+++ src/highcharts/column.ts
@@ -55,19 +55,75 @@ const chart = Highcharts.chart({
     type: 'datetime',
   },
   yAxis: {
-    tickInterval: 5,
     min: 0,
     max: 20,
+    tickPositioner() {
+      /**
+       * 動的にtickPositionを更新したい場合は setPositioner を使う
+       */
+      return [
+        this.min,
+        Math.round((this.max - this.min) / 2 + this.min),
+        this.max,
+      ];
+    },
     title: {
       text: '稼働時間',
     },
     labels: {
       format: '{value}分',
     },
+    plotLines: [
+      {
+        id: 'from',
+        color: 'blue',
+        dashStyle: 'LongDash',
+        value: 0,
+        width: 2,
+        zIndex: 5,
+      },
+      {
+        id: 'to',
+        color: 'red',
+        dashStyle: 'LongDash',
+        value: 20,
+        width: 2,
+        zIndex: 5,
+      },
+    ],
+  },
+  plotOptions: {
+    series: {
+      animation: false,
+    },
   },
   series,
 });
 
+/**
+ * y軸のmin,maxを更新する
+ */
 document.querySelector('#modifyColumnAxis').addEventListener('click', () => {
-  chart.yAxis[0].setExtremes(5, 15);
+  const min = 5;
+  const max = 15;
+  const yAxis = chart.yAxis[0];
+  yAxis.setExtremes(min, max);
+  yAxis.removePlotLine('from');
+  yAxis.removePlotLine('to');
+  yAxis.addPlotLine({
+    id: 'from',
+    color: 'blue',
+    dashStyle: 'LongDash',
+    value: min,
+    width: 2,
+    zIndex: 5,
+  });
+  yAxis.addPlotLine({
+    id: 'to',
+    color: 'red',
+    dashStyle: 'LongDash',
+    value: max,
+    width: 2,
+    zIndex: 5,
+  });
 });
